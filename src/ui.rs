aa8d8352969de580ebe3080764de59eb1b7098a1
Rob Parrett <robparrett@gmail.com>
2022-08-22 08:58:47 -0700

    Add race timer ui

--- src/ui.rs
+++ src/ui.rs
@@ -1,14 +1,18 @@
 use bevy::prelude::*;
 use interpolation::Ease;
 
-use crate::{GameAssets, GameState};
+use crate::{GameAssets, GameState, RaceTime};
 
 pub struct UiPlugin;
 impl Plugin for UiPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<TrickTextTimer>()
             .add_system_set(SystemSet::on_enter(GameState::Playing).with_system(setup))
-            .add_system_set(SystemSet::on_update(GameState::Playing).with_system(fade_trick_text));
+            .add_system_set(
+                SystemSet::on_update(GameState::Playing)
+                    .with_system(fade_trick_text)
+                    .with_system(race_time),
+            );
     }
 }
 
@@ -21,8 +25,47 @@ impl Default for TrickTextTimer {
         Self(Timer::from_seconds(2., false))
     }
 }
+#[derive(Component)]
+pub struct RaceTimeText;
 
 fn setup(mut commands: Commands, assets: Res<GameAssets>) {
+    commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                position_type: PositionType::Absolute,
+                position: UiRect {
+                    right: Val::Px(5.),
+                    top: Val::Px(5.),
+                    ..default()
+                },
+                size: Size {
+                    width: Val::Px(120.),
+                    height: Val::Px(60.),
+                    ..default()
+                },
+                align_items: AlignItems::FlexEnd,
+                justify_content: JustifyContent::FlexStart,
+                ..Default::default()
+            },
+            color: Color::NONE.into(),
+            ..default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn_bundle(TextBundle {
+                    text: Text::from_section(
+                        "0.000",
+                        TextStyle {
+                            font: assets.font.clone(),
+                            font_size: 60.0,
+                            color: Color::WHITE,
+                        },
+                    ),
+                    ..Default::default()
+                })
+                .insert(RaceTimeText);
+        });
+
     commands
         .spawn_bundle(TextBundle {
             style: Style {
@@ -90,4 +133,14 @@ pub fn trick_text(front_flips: u32, back_flips: u32) -> String {
     }
 
     lines.join("\n")
+}
+
+fn race_time(time: Res<RaceTime>, mut query: Query<&mut Text, With<RaceTimeText>>) {
+    if !time.is_changed() {
+        return;
+    }
+
+    for mut text in query.iter_mut() {
+        text.sections[0].value = format!("{:.3}", time.elapsed_secs());
+    }
 }
